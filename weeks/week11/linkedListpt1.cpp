6b53be13ede73d5321d5315c9071e0fb2711f655
nosey-dewdrop <damummyphus@gmail.com>
2025-11-25 16:11:56 +0300

    additions

--- weeks/week11/linkedListpt1.cpp
+++ weeks/week11/linkedListpt1.cpp
@@ -2,44 +2,57 @@
 using namespace std;
 
 struct Node {
-    int data;       
-    Node* next;     
+      int data;       
+      Node* next;     
 };
 
 int main() {
-    Node* n1 = new Node();
-    n1->data = 10;
+      Node* n1 = new Node();
+      n1->data = 10;
 
-    Node* n2 = new Node();
-    n2->data = 20;        
+      Node* n2 = new Node();
+      n2->data = 20;        
 
-    Node* n3 = new Node();
-    n3->data = 30; 
-    
-    Node* head = n1; // node türünden bir pointer.
-    n1 -> next = n2;
-    n2 -> next = n3;
-    n3 -> next = nullptr;
+      Node* n3 = new Node();
+      n3->data = 30; 
 
-    Node* temp = head; //temporary node.
-    cout << head << endl;
-    cout << &head << endl;
+      Node* head = n1; // node türünden bir pointer.
+      n1 -> next = n2;
+      n2 -> next = n3;
+      n3 -> next = nullptr;
 
-    cout << temp << endl;
-    cout << &temp << endl;
+      Node* temp = head; //temporary node.
+      cout << head << endl;
+      cout << &head << endl;
 
-    while(temp != nullptr){
-      cout << temp -> data << " : " << endl;
-      cout << "temp initial:" << temp << endl;
-      cout << "head initial:" << head << endl;
-      cout << "temp initial: (is it n1?)" << n1 << endl; //yes
+      cout << temp << endl;
+      cout << &temp << endl;
 
-      temp = temp -> next;
-      
-      cout << "temp final:" << temp << endl;
-      cout << "temp final: (is it n2?)" << n2 << endl; //yes
-    }
+      temp = head;
+      while(temp != nullptr){
+            cout << temp -> data << " -> " ;
+            // cout << "temp initial:" << temp << endl;
+            // cout << "head initial:" << head << endl;
+            // cout << "temp initial: (is it n1?)" << n1 << endl; //yes
 
-    cout << "end!" << endl;
-    return 0;
+            temp = temp -> next;
+
+            // cout << "temp final:" << temp << endl;
+            // cout << "temp final: (is it n2?)" << n2 << endl; //yes
+      }
+
+
+      cout << "\nnow add a new number to the seond position!!"<< endl;
+      Node* addItem = new Node();
+      addItem -> data = 40;
+      addItem -> next = n2;
+      n1 -> next = addItem;
+
+      temp = head;
+      while(temp != nullptr){
+            cout << temp -> data << " -> " ;
+            temp = temp -> next;
+      }
+      cout << "\nend!" << endl;
+      return 0;
 }
