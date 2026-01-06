6d2606b8bb077436f831a4e209a6ad601ca27ed1
nosey-dewdrop <damummyphus@gmail.com>
2026-01-06 05:07:39 +0300

    okkkayy

---
+++ RemoveElement.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+
+
+public class RemoveElement {
+
+      public static int removeElement(int[] nums, int val) {
+            int i = 0; 
+
+            for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                  nums[i] = nums[j];
+                  i++;
+            }
+            }
+
+            return i; 
+      }
+      public static void main(String[] args) {
+
+            int[] nums = {0,1,2,2,3,0,4,2};
+            int val = 2;
+
+            int k = removeElement(nums, val);
+
+            System.out.println("k = " + k);
+
+            System.out.print("nums = [");
+            for (int i = 0; i < k; i++) {
+                  System.out.print(nums[i]);
+                  if (i < k - 1){
+                        System.out.print(", ");
+                  } 
+            }
+            System.out.println("]");
+      }
+}
