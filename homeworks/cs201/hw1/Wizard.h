e07da924760b7252caafa1cd8a33db74c8745d71
nosey-dewdrop <damummyphus@gmail.com>
2026-02-16 00:54:09 +0300

    and in her eyes you see nothing

---
+++ homeworks/cs201/hw1/Wizard.h
@@ -0,0 +1,15 @@
+
+#ifndef WIZARDPOTIONINVENTORYSYSTEM_H
+#define WIZARDPOTIONINVENTORYSYSTEM_H
+
+#include <string>
+using namespace std;
+
+// structs are public by default unlike classes in cpp
+struct Potion {
+      string potionName;
+      int strength;
+};
+
+
+#endif
